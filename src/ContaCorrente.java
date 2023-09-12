fc7a3d0335220e621e750ce2e04c414e61c68576
Caio Zamana <caiobzm@gmail.com>
2023-09-12 17:22:26 -0300

    Initial commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,104 @@
+public class ContaCorrente {
+
+    private int numeroConta;
+    private int numeroAgencia;
+    private String nomeDoTitular;
+    private String cpfCnpj;
+    private double saldo;
+    private double limiteChequeEspecial;
+
+    ContaCorrente() {
+    }
+
+    public ContaCorrente(int numeroConta, int agencia, String nomeDoTitular, String cpfCnpj) {
+        this.numeroConta = numeroConta;
+        this.nomeDoTitular = nomeDoTitular;
+        this.cpfCnpj = cpfCnpj;
+        this.numeroAgencia = agencia;
+    }
+
+    public void sacar(double valorSaque) {
+        if (valorSaque <= this.saldo) {
+            this.saldo -= valorSaque;
+        }
+    }
+
+    public void sacar(double valorSaque, String motivoDoSaque) {
+        if (valorSaque <= this.saldo) {
+            this.saldo -= valorSaque;
+        }
+    }
+    public void depositar(double valorDeposito) {
+        if (valorDeposito > 0) {
+            this.saldo += valorDeposito;
+        }
+    }
+
+    public void depositar(double valorDeposito, String motivoDoSaque) {
+        if (valorDeposito > 0) {
+            this.saldo += valorDeposito;
+        }
+    }
+
+    public void transferir(ContaCorrente contaDestino, double valorTransferencia) {
+        if (valorTransferencia <= 0) {
+            throw new IllegalArgumentException("O valor da transferência está inválido. O valor deve ser positivo.");
+        }
+        if (this.saldo < valorTransferencia) {
+            throw new IllegalArgumentException("Saldo Insuficiente");
+        }
+
+        this.saldo -= valorTransferencia;
+        contaDestino.setSaldo(contaDestino.getSaldo() + valorTransferencia);
+
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public int getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(int numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public String getNomeDoTitular() {
+        return nomeDoTitular;
+    }
+
+    public void setNomeDoTitular(String nomeDoTitular) {
+        this.nomeDoTitular = nomeDoTitular;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.text.DecimalFormat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContaCorrente conta1 = new ContaCorrente(777, 1, "Luan Rosa", "123.456.789-00");
+        ContaCorrente conta2 = new ContaCorrente(369, 1, "Maria Poeta", "987.654.321-00");
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        System.out.println("Saldo inicial da conta 1: " + conta1.getSaldo());
+
+
+        conta1.setSaldo(1000.0);
+        System.out.println("Novo saldo da conta 1 após setar o saldo: " + decimalFormat.format(conta1.getSaldo()));
+        conta1.depositar(200);
+        System.out.println("Novo saldo da conta 1 após depositar mais saldo: " + decimalFormat.format(conta1.getSaldo()));
+        conta1.sacar(100);
+        System.out.println("Novo saldo da conta 1 após sacar saldo: " + decimalFormat.format(conta1.getSaldo()));
+
+
+        System.out.println("\nSaldo da inicial conta 2: " + decimalFormat.format(conta2.getSaldo()));
+        conta2.setSaldo(500.0);
+        System.out.println("Novo saldo da conta 2 após setar o saldo: " + decimalFormat.format(conta2.getSaldo()));
+
+        double valorTransferencia = 100;
+        conta1.transferir(conta2, valorTransferencia);
+        System.out.println("\nTransferência da conta 1 de R$" + valorTransferencia + " para conta2 numero: " + conta2.getNumeroConta());
+        System.out.println("Saldo atual da conta 1 após transferência: " + decimalFormat.format(conta1.getSaldo()));
+        System.out.println("Saldo atual da conta 2 após recebimento de transferência: " + decimalFormat.format(conta2.getSaldo()));
+
+
+        System.out.println("\nInformações da conta 1:");
+        System.out.println("Número da Conta: " + conta1.getNumeroConta());
+        System.out.println("Nome do Titular: " + conta1.getNomeDoTitular());
+        System.out.println("CPF/CNPJ: " + conta1.getCpfCnpj());
+        System.out.println("Saldo: " + decimalFormat.format(conta1.getSaldo()));
+
+        System.out.println("\nInformações da conta 2:");
+        System.out.println("Número da Conta: " + conta2.getNumeroConta());
+        System.out.println("Nome do Titular: " + conta2.getNomeDoTitular());
+        System.out.println("CPF/CNPJ: " + conta2.getCpfCnpj());
+        System.out.println("Saldo: " + decimalFormat.format(conta2.getSaldo()));
+
+
+        /*
+        conta1.transferir(conta2, -100);
+        */
+
+        /*
+        conta1.transferir(conta2, 10000);
+        */
+    }
+}
